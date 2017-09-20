738cbaf57299bd342803eedbf74668c84ed6a0c1
Cassandra <poisonberryvines@gmail.com>
2017-09-20 09:47:03 -0400

    Finished .cpp file version

--- randomNumFile.cpp
+++ randomNumFile.cpp
@@ -1,24 +1,50 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <fstream>
 
 using namespace std;
+//creates and returns a random character
+char randomChar(){
 
-char* randomNum(){}
-
+  int randInt = rand() % 26;
+  //look at me cheat lmao
+  return "abcdefghijklmnopqrstuvwxyz"[randInt];
+}
+//arguments follow the following order: filename, number of characters
 int main(int argc, char *argv[]){
 
-  if(argc < 3){
-    cout << "Not enough arguments!" << endl;
+  if(argc < 2){
+    std::cout << "Not enough arguments!" << std::endl;
+  }
+  else if(argc > 2){
+    std::cout << "Too many arguments!" << std::endl;
   }
 
   else{
     //take first argument as the file name and write to it
     ofstream testFile;
-    testFile.open(argv[0]);
+    testFile.open(argv[1]);
 
-    
+    if(!testFile.is_open()){
+      std::cout << "Invalid file!" << std::endl;
+    }
+    else{
+      //seed random number generator for program
+      //only does this after program is initialized properly
+      srand(time(NULL));
+
+      int i = atoi(argv[2]);
+
+      for(int n = 0; n <= i; n++){
+        testFile << randomChar() << " ";
+      }
+
+      testFile << "\n";
+
+      testFile.close();
+    }
   }
 
   return 0;
